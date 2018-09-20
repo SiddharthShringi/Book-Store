4bf8bf9f792b52ddfa507c7618de339cdb8f1c02
BlackSamsung <siddharthshringi@gmail.com>
2018-09-20 12:47:48 +0530

    add id book-list

--- crud.js
+++ crud.js
@@ -2,8 +2,27 @@
 
 var addBook = document.getElementById('add-book');
 
+
+counter = 0;
 addBook.addEventListener('click', event => {
     var data = document.getElementById('data');
     var inputData = data.value;
-    console.log(inputData);
+    var bookList = document.getElementById('book-list');
+
+    var listHtml = '<div class="row" id="data'+ counter +'">'+
+    '<div class="col-md-6">'+
+        '<input type="text" class="form-control">'+
+    '</div>'+
+    '<div class="col-sm-2">'+
+        '<button type="button" class="btn btn-success">Edit</button>'+
+    '</div>'+
+    '<div class="col-sm-2">'+
+        '<button type="button" class="btn btn-success">Update</button>'+
+    '</div>'+
+    '<div class="col-sm-2">'+
+        '<button type="button" class="btn btn-danger">Delete</button>'+
+    '</div>'+
+'</div>'
+    counter++;
+    bookList.insertAdjacentHTML('beforeend', listHtml);
 });
