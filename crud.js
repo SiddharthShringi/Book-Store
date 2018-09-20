b7904c5b7e98e27ce0f4375720fa54fc61a7b503
BlackSamsung <siddharthshringi@gmail.com>
2018-09-20 11:20:14 +0530

    make function to read input field data

--- crud.js
+++ crud.js
@@ -1,1 +1,9 @@
-alert("hello world")
+
+
+var addBook = document.getElementById('add-book');
+
+addBook.addEventListener('click', event => {
+    var data = document.getElementById('data');
+    var inputData = data.value;
+    console.log(inputData);
+});
